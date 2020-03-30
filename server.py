3070a8c61ea01b8c7d6d4e4e2832a4fa0c5a2d79
TheSpeedX <ggspeedx29@gmail.com>
2020-03-30 10:45:24 +0530

    done index

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask , request, render_template, Markup
 import requests
 from datetime import datetime
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder=".",static_folder='assets')
 
 @app.route('/')
 def index():

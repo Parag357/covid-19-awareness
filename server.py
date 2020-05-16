bcda92586af5d74911c4c8ef10f2a126d4af1b38
TheSpeedX <ggspeedx29@gmail.com>
2020-05-16 19:04:55 +0530

    auto update

--- server.py
+++ server.py
@@ -4,22 +4,37 @@ from datetime import datetime
 from bs4 import BeautifulSoup
 import os
 import json
+import pickle
+import scrap
 app = Flask(__name__,template_folder=".",static_folder='assets')
 
 @app.route('/')
 def index():
+	update=False
+	now=datetime.now()
+	if not os.path.exists('update.save'):
+		update=True
+	else:
+		prev=pickle.load(open('update.save','rb'))
+		duration=(now-prev).total_seconds()
+		duration=divmod(duration,3600)[0]
+		update=duration>=6
+	if update:
+		scrap.save_data()
+		with open('update.save', 'wb') as update_file:
+			pickle.dump(now, update_file)
 	news_list=json.load(open("news.save","r"))['news']
 	news_format="""
 	    <div class="row">
-      <div class="col-sm-2" align="right">
-        <span class="badge badge-dark badge-pill">{time}</span>
-      </div>
-      <div class="col-sm-10">
-        <div class="card44 ">
+	  <div class="col-sm-2" align="right">
+		<span class="badge badge-dark badge-pill">{time}</span>
+	  </div>
+	  <div class="col-sm-10">
+		<div class="card44 ">
 		<a href="{link}" alt="Link To Article" target="_blank">{title}</a> 
-        </div>
-      </div>
-    </div>"""
+		</div>
+	  </div>
+	</div>"""
 	news_html="\n\n".join([news_format.format(title=news["title"],link=news["link"],time=news["time"]) for news in news_list ])
 	total_stats=json.load(open("stats.save","r"))
 	world_stats=total_stats['world']

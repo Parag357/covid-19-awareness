c788c367c56f3e1708ee8dd63c0f72e47075f322
Parag <psychoprg357@gmail.com>
2020-08-03 16:29:30 +0530

    trying annotations

--- updater.py
+++ updater.py
@@ -46,17 +46,15 @@ def save_world():
 	data=dict(type='choropleth',locations=df['CODE'],z=df['active'],showscale=False,colorscale='redor',
          text=df['text'],marker=dict(line=dict(color='#2d383a',width=1)))
 	layout=dict(geo=dict(showocean=True,oceancolor='lightblue', showframe=False, projection={'type':'orthographic'}))
-	fig.update_layout(showlegend=False,annotations=[dict(go.layout.Annotation(
+	fig.update_layout(showlegend=False,annotations=[dict(
                 text='Some<br>multi-line<br>text',
                 align='left',
                 showarrow=False,
-                xref='paper',
-                yref='paper',
                 x=1.1,
                 y=0.8,
                 bordercolor='black',
                 borderwidth=0
-            ))])
+            )])
 	chormap=go.Figure([data],layout)
 	chormap.write_html('world.html')
 def save_data():
